d8a0501a69fb6d5de6b9b959cde223e096e36d85
amy <amyemartyn@gmail.com>
2018-05-31 14:47:43 -0700

    basic functionality

--- maap.py
+++ maap.py
@@ -1,4 +1,4 @@
-
+import json
 import aiohttp
 from aiohttp import web
 
@@ -8,19 +8,30 @@ routes = web.RouteTableDef()
 async def hello(request):
     return web.Response(text="Hello, world")
 
+sockets = []
+state = json.dumps([[0 for column in range(20)] for row in range(20)])
+
 @routes.get('/ws')
 async def websocket_handler(request):
+    global state
 
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-
+    sockets.append(ws)
+    if state:
+        await ws.send_str(state)
+    
     async for msg in ws:
         if msg.type == aiohttp.WSMsgType.TEXT:
             if msg.data == 'close':
                 await ws.close()
             else:
                 print(msg.data)
-                await ws.send_str(msg.data + '/answer')
+                state = msg.data
+                for socket in sockets:
+                    if socket is ws:
+                        continue
+                    await socket.send_str(msg.data)
         elif msg.type == aiohttp.WSMsgType.ERROR:
             print('ws connection closed with exception %s' %
                   ws.exception())
