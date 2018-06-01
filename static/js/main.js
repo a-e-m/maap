fb0cd0f08497183ff12910ff7269e2296ae95997
amy <amyemartyn@gmail.com>
2018-06-01 15:30:58 -0700

    tweaks to UI and efficiency

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,28 @@
 
 var players = ['repeating-linear-gradient(45deg,#606dbc,#606dbc 10px,#465298 10px,#465298 20px)',
 'repeating-linear-gradient(45deg,yellow,yellow 10px,green 10px,green 20px)',
-'repeating-linear-gradient(45deg,red,red 10px,orange 10px,orange 20px)'];
+'repeating-linear-gradient(45deg,red,red 10px,orange 10px,orange 20px)',
+'radial-gradient(circle, rgba(234,244,0,1) 0%, rgba(228,10,10,1) 100%)',
+'radial-gradient(circle, rgba(0,244,28,1) 0%, rgba(10,161,228,1) 100%)',
+'radial-gradient(circle, rgba(131,3,131,1) 0%, rgba(0,38,244,1) 100%)',];
+
+var effects = ['linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,212,255,0.19089642693014708) 100%)', 
+'linear-gradient(90deg, rgba(242,8,8,1) 0%, rgba(255,198,5,0.8127451664259454) 100%)',
+'linear-gradient(90deg, rgba(251,173,3,1) 0%, rgba(255,248,0,0.3225490879945728) 100%)',
+'linear-gradient(90deg, rgba(3,251,69,1) 0%, rgba(142,255,0,0.37296925606179976) 100%)',
+'linear-gradient(90deg, rgba(3,245,251,0.8183474073223039) 0%, rgba(241,255,253,0.37296925606179976) 100%)',
+'linear-gradient(90deg, rgba(255,255,255,0.8519608527004552) 0%, rgba(241,255,253,0.37296925606179976) 100%)'];
+
+var bg = [
+];
 
 // state and data:
 var state = {
     tool: 'brush',
-    colors: ['rgba(0, 0, 0, 0)', 'black', 'white', 'red', 'maroon', 'orange', 'yellow', 'green', '#01410f', 'skyblue', 'blue', 'purple', '#4a2e13', 'tan', '#444', '#ccc'].concat(players),
+    colors: ['rgba(0, 0, 0, 0)', 'black', 'white', 'red', 'maroon', 'orange', 'yellow', 'green', '#01410f', 'skyblue', 'blue', 'purple', '#4a2e13', 'tan', '#444', '#ccc'].concat(players).concat(effects),
     color: 1,
     layer: +$('.layer:checked').attr('value'),
-    wsUri: "ws://localhost:8080/ws",
+    wsUri: "ws://" + window.location.host + "/ws",
     mapData: [[[0, 0, 0], [0, 0, 0], [0, 0, 0]]],
     tileSize: 64
 };
@@ -40,7 +53,8 @@ _.each(state.colors, function(color, index) {
 
 var toolStates = {
     brush: {
-        down: false
+        down: false,
+        erase: false
     },
     move: {
         start: null
@@ -55,22 +69,33 @@ $('.tool').click(function(event) {
 });
 
 $('#map').mousedown(handleTool);
-$(window).mouseup(handleTool).blur(handleTool);
+$(window).mouseup(handleTool).blur(handleTool).on('contextmenu', handleTool);
 
 function handleBrush(event) {
+    var color;
     if (event.type === 'mousedown') {
         toolStates.brush.down = true;
         $('#map').mousemove(handleTool);
-        changeSquare($(event.target));
+        if (event.which == 3) {
+            color = 0;
+            toolStates.brush.erase = true;
+        }
+        changeSquare($(event.target), color);
     } else if (event.type === 'mousemove') {
         if (toolStates.brush.down) {
-            changeSquare($(event.target));
+            if (toolStates.brush.erase)
+                color = 0;
+            changeSquare($(event.target), color);
         }
     }
     else if (event.type === 'mouseup' || event.type === 'blur') {
         toolStates.brush.down = false;
+        toolStates.brush.erase = false;
         $('#map').off('mousemove');
         sendMap();
+    } else {
+        event.preventDefault();
+        event.stopPropagation();
     }
 }
 
@@ -78,7 +103,6 @@ function handleMove(event) {
     if (event.type === 'mousedown') {
         $('#map').mousemove(handleTool);
         toolStates.move.start = $(event.target);
-        console.log('saved', event.target);
     } else if (event.type === 'mouseup' || event.type === 'blur') {
         var start = toolStates.move.start;
         var end = $(event.target);
@@ -174,11 +198,22 @@ function getColor(elem) {
     return state.mapData[state.layer][y][x];
 }
 
+function getSquareId(layer, y, x) {
+    return 'square' + layer + '-' + y + '-' + x;
+}
+
 function changeSquare(elem, color) {
     var x = gridX(parseInt(elem.css('left'))),
         y = gridY(parseInt(elem.css('top')));
-    state.mapData[state.layer][y][x] = (color === undefined) ? state.color : color;
-    makeMap(state.mapData, state.layer);
+    if (color === undefined) {
+        color = state.color;
+    }
+    state.mapData[state.layer][y][x] = color;
+    var elem = $('#' + getSquareId(state.layer, y, x));
+    elem
+        .css('background', state.colors[color])
+        .data('colorIndex', color);
+    //makeMap(state.mapData, state.layer);
 }
 
 function makeMapOld(data, layer) {
@@ -193,7 +228,8 @@ function makeMapOld(data, layer) {
                 .css('top', pixelY(y))
                 .addClass('layer' + layer)
                 .data('layerIndex', layer)
-                .data('colorIndex', item);
+                .data('colorIndex', item)
+                .attr('id', getSquareId(layer, y, x));
             $('#map').append(square);
         });
     });
